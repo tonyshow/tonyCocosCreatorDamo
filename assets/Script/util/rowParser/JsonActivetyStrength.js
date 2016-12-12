9e45109a28c906c06c54225254de4e2eaa706af3
tony <994262676@qq.com>
2016-12-12 20:57:46 +0800

    转表优化
    转表优化

--- assets/Script/util/rowParser/JsonActivetyStrength.js
+++ assets/Script/util/rowParser/JsonActivetyStrength.js
@@ -5,7 +5,7 @@ var JosnActivetyStrength = function () {
 
 	var data = [[1,1,12,2,7101],[2,1,18,2,7101]];
 
-	var indexs = {1:3,2:4};
+	var indexs = {"1":3,"2":4};
 
 	var indexNames = {"id":0,"group":1,"openingTime":2,"lastTime":3,"rewardId":4};
 
--- packages/excel2js/excel2json.bat
+++ packages/excel2js/excel2json.bat
@@ -1,3 +1,3 @@
 excel2json.py .\in .\out
-xcopy  out .\..\..\assets\Script\util\rowParser\
+xcopy /y out .\..\..\assets\Script\util\rowParser\
 pause
