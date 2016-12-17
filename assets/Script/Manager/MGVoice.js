e9b556d2c13c8fddecc220be96d6dc008937a929
tony <994262676@qq.com>
2016-12-17 10:04:05 +0800

    声音管理类添加

---
+++ assets/Script/Manager/MGVoice.js
@@ -0,0 +1,23 @@
+/*
+* 音乐、音效管理
+* 作者 ：tony
+* 时间 ：2016-12-17
+*/
+var MGVoice = module.exports = {};
+var pro = MGVoice.prototype;
+
+/**
+ * 播放音乐
+ */
+pro.playMusic = function(){
+
+};
+
+/**
+ * 播放音效
+ */
+pro.playSound = function(){
+
+};
+
+window.MGVoice = MGVoice;
--- packages/excel2js/main.js
+++ packages/excel2js/main.js
@@ -4,7 +4,7 @@ var Path = require('path');
 module.exports = {
     messages: { 
         parse () { 
-             cc.log('执行转表请执行packages/excel2js/excel2json.bat');              
+             cc.error('执行转表请执行packages/excel2js/excel2json.bat');                       
         }
     }
 };
