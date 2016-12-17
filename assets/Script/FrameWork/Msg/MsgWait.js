37675d5c68dd823c4605a9bf6c8e62e80e2716b2
tony <994262676@qq.com>
2016-12-17 18:37:00 +0800

    修改框架文档

--- assets/Script/FrameWork/Msg/MsgWait.js
+++ assets/Script/FrameWork/Msg/MsgWait.js
@@ -15,16 +15,32 @@ window.MsgWait = cc.Class({
     },
 
     update : function(){
-        if(this.isWaitTimeOut()){
-            MsgPrompt.createByGameWorld('com_timeout');
-            this.close();
+        if( this.isWaitTimeOut() && false==this.isEnd ){
+            this.isEnd = true; 
+            this.doActionClose();
         }
+    },
+
+    close : function(){
+         MsgPrompt.createByGameWorld('com_timeout');         
+         this._super(); 
+    },
+
+    doActionClose : function(){
+        var action = cc.fadeTo(0.2, 0);
+        var self = this;
+        var finish = cc.callFunc(function(){
+            self.close();
+        });
+        var seq = cc.sequence( cc.fadeTo(1.0, 0),finish ); 
+        self.node.runAction( seq ); 
     },
 
     onLoad : function(){
         this._super();
         this.startTime = new Date();  
         this.waitTime = PlanApi.PlanGameConfig.getValue('waitTime');
+        this.isEnd = false;
     },
 
     //是否连接超时
